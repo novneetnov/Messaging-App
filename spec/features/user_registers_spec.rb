6be7406618768aa49bff122aab9d236b1f8b5ff5
novneetnov <novneet346iith@gmail.com>
2016-10-02 12:18:25 +0530

    Install Devise and Add User model

--- spec/features/user_registers_spec.rb
+++ spec/features/user_registers_spec.rb
@@ -1,1 +1,15 @@
+require 'rails_helper'
 
+describe 'user registers an account' do
+	it 'successfully' do
+		visit new_user_registration_path
+
+		fill_in 'Username', with: 'username'
+		fill_in 'Email', with: 'test@test.com'
+		fill_in 'Password', with: 'password'
+		fill_in 'Password Confirmation', with: 'password'
+		click_button 'Sign up'
+
+		expect(page).to have_content 'Welcome you have signed up successfully'
+	end
+end
